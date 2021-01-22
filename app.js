d85a0f7b38a89305b4c007fe2459d6a8676ae3fa
Cdric-art <cdric.art@gmail.com>
2021-01-22 16:14:38 +0100

    Config

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const commentRoutes = require('./routes/comment.routes')
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api', commentRoutes);
+
+module.exports = app;
---
+++ controllers/comment.controller.js
@@ -0,0 +1,15 @@
+module.exports.getAllComments = async (req, res) => {
+
+}
+
+module.exports.getComment = async (req, res) => {
+
+}
+
+module.exports.setComment = async (req, res) => {
+
+}
+
+module.exports.deleteComment = async (req, res) => {
+
+}
